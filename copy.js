62d8140f30f034e91f0ff3c5979006eaaa5e0c2f
wanglin <1013335014@qq.com>
2021-07-14 08:13:23 +0800

    修改配置

--- copy.js
+++ copy.js
@@ -4,13 +4,11 @@ const path = require('path')
 const src = path.resolve(__dirname, './dist/index.html') 
 const dest = path.resolve(__dirname, './index.html') 
 
-const destStat = fs.statSync(dest)
-if (destStat.isFile()) {
+if (fs.existsSync(dest)) {
     fs.unlinkSync(dest)
 }
 
-const srcStat = fs.statSync(src)
-if (srcStat.isFile()) {
+if (fs.existsSync(src)) {
     fs.copyFileSync(src, dest)
     fs.unlinkSync(src)
 }
