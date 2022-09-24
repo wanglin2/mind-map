367d9cb632229352c65e0c52bb7db863a1e87751
wanglin2 <1013335014@qq.com>
2022-09-24 20:42:53 +0800

    打包

--- simple-mind-map/dist/simpleMindMap.umd.js
+++ simple-mind-map/dist/simpleMindMap.umd.js
@@ -22893,7 +22893,7 @@ class Node_Node {
       return;
     }
 
-    e.stopPropagation();
+    e && e.stopPropagation();
 
     if (this.nodeData.data.isActive) {
       return;
@@ -22977,6 +22977,13 @@ class Node_Node {
           item.render();
         };
       }));
+    } // 手动插入的节点立即获得焦点并且开启编辑模式
+
+
+    if (this.nodeData.inserting) {
+      delete this.nodeData.inserting;
+      this.active();
+      this.mindMap.emit('node_dblclick', this);
     }
   }
   /** 
@@ -23064,7 +23071,7 @@ class Node_Node {
    */
 
 
-  renderLine() {
+  renderLine(deep = false) {
     if (this.nodeData.data.expand === false) {
       return;
     }
@@ -23089,10 +23096,12 @@ class Node_Node {
     this.renderer.layout.renderLine(this, this._lines, (line, node) => {
       // 添加样式
       this.styleLine(line, node);
-    }); // 和父级的连线也需要更新
+    }); // 子级的连线也需要更新
 
-    if (this.parent) {
-      this.parent.renderLine();
+    if (deep && this.children && this.children.length > 0) {
+      this.children.forEach(item => {
+        item.renderLine(deep);
+      });
     }
   }
   /** 
@@ -25272,7 +25281,139 @@ class TextEdit_TextEdit {
   }
 
 }
+// CONCATENATED MODULE: ../simple-mind-map/src/themes/default.js
+/** 
+ * @Author: 王林 
+ * @Date: 2021-04-11 10:19:55 
+ * @Desc: 默认主题 
+ */
+/* harmony default export */ var themes_default = ({
+  // 节点内边距
+  paddingX: 15,
+  paddingY: 5,
+  // 图片显示的最大宽度
+  imgMaxWidth: 100,
+  // 图片显示的最大高度
+  imgMaxHeight: 100,
+  // icon的大小
+  iconSize: 20,
+  // 连线的粗细
+  lineWidth: 1,
+  // 连线的颜色
+  lineColor: '#549688',
+  // 连线样式
+  lineDasharray: 'none',
+  // 概要连线的粗细
+  generalizationLineWidth: 1,
+  // 概要连线的颜色
+  generalizationLineColor: '#549688',
+  // 概要曲线距节点的距离
+  generalizationLineMargin: 0,
+  // 概要节点距节点的距离
+  generalizationNodeMargin: 20,
+  // 背景颜色
+  backgroundColor: '#fafafa',
+  // 背景图片
+  backgroundImage: 'none',
+  // 背景重复
+  backgroundRepeat: 'no-repeat',
+  // 根节点样式
+  root: {
+    shape: 'rectangle',
+    fillColor: '#549688',
+    fontFamily: '微软雅黑, Microsoft YaHei',
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    lineHeight: 1.5,
+    borderColor: 'transparent',
+    borderWidth: 0,
+    borderDasharray: 'none',
+    borderRadius: 5,
+    textDecoration: 'none',
+    active: {
+      borderColor: 'rgb(57, 80, 96)',
+      borderWidth: 3,
+      borderDasharray: 'none'
+    }
+  },
+  // 二级节点样式
+  second: {
+    shape: 'rectangle',
+    marginX: 100,
+    marginY: 40,
+    fillColor: '#fff',
+    fontFamily: '微软雅黑, Microsoft YaHei',
+    color: '#565656',
+    fontSize: 16,
+    fontWeight: 'noraml',
+    fontStyle: 'normal',
+    lineHeight: 1.5,
+    borderColor: '#549688',
+    borderWidth: 1,
+    borderDasharray: 'none',
+    borderRadius: 5,
+    textDecoration: 'none',
+    active: {
+      borderColor: 'rgb(57, 80, 96)',
+      borderWidth: 3,
+      borderDasharray: 'none'
+    }
+  },
+  // 三级及以下节点样式
+  node: {
+    shape: 'rectangle',
+    marginX: 50,
+    marginY: 0,
+    fillColor: 'transparent',
+    fontFamily: '微软雅黑, Microsoft YaHei',
+    color: '#6a6d6c',
+    fontSize: 14,
+    fontWeight: 'noraml',
+    fontStyle: 'normal',
+    lineHeight: 1.5,
+    borderColor: 'transparent',
+    borderWidth: 0,
+    borderRadius: 5,
+    borderDasharray: 'none',
+    textDecoration: 'none',
+    active: {
+      borderColor: 'rgb(57, 80, 96)',
+      borderWidth: 3,
+      borderDasharray: 'none'
+    }
+  },
+  // 概要节点样式
+  generalization: {
+    shape: 'rectangle',
+    marginX: 100,
+    marginY: 40,
+    fillColor: '#fff',
+    fontFamily: '微软雅黑, Microsoft YaHei',
+    color: '#565656',
+    fontSize: 16,
+    fontWeight: 'noraml',
+    fontStyle: 'normal',
+    lineHeight: 1.5,
+    borderColor: '#549688',
+    borderWidth: 1,
+    borderDasharray: 'none',
+    borderRadius: 5,
+    textDecoration: 'none',
+    active: {
+      borderColor: 'rgb(57, 80, 96)',
+      borderWidth: 3,
+      borderDasharray: 'none'
+    }
+  }
+}); // 支持激活样式的属性
+// 简单来说，会改变节点大小的都不支持在激活时设置，为了性能考虑，节点切换激活态时不会重新计算节点大小
+
+const supportActiveStyle = ['fillColor', 'color', 'fontWeight', 'fontStyle', 'borderColor', 'borderWidth', 'borderDasharray', 'borderRadius', 'textDecoration'];
+const lineStyleProps = ['lineColor', 'lineDasharray', 'lineWidth'];
 // CONCATENATED MODULE: ../simple-mind-map/src/Render.js
+
 
 
 
@@ -25714,6 +25855,7 @@ class Render_Render {
 
       let index = this.getNodeIndex(first);
       first.parent.nodeData.children.splice(index + 1, 0, {
+        "inserting": true,
         "data": {
           "text": text,
           "expand": true
@@ -25742,6 +25884,7 @@ class Render_Render {
 
       let text = node.isRoot ? '二级节点' : '分支主题';
       node.nodeData.children.push({
+        "inserting": true,
         "data": {
           "text": text,
           "expand": true
@@ -26082,7 +26225,11 @@ class Render_Render {
       };
     }
 
-    this.setNodeDataRender(node, data);
+    this.setNodeDataRender(node, data); // 更新了连线的样式
+
+    if (lineStyleProps.includes(prop)) {
+      (node.parent || node).renderLine(true);
+    }
   }
   /** 
    * @Author: 王林 
@@ -26424,136 +26571,6 @@ class Render_Render {
 }
 
 /* harmony default export */ var src_Render = (Render_Render);
-// CONCATENATED MODULE: ../simple-mind-map/src/themes/default.js
-/** 
- * @Author: 王林 
- * @Date: 2021-04-11 10:19:55 
- * @Desc: 默认主题 
- */
-/* harmony default export */ var themes_default = ({
-  // 节点内边距
-  paddingX: 15,
-  paddingY: 5,
-  // 图片显示的最大宽度
-  imgMaxWidth: 100,
-  // 图片显示的最大高度
-  imgMaxHeight: 100,
-  // icon的大小
-  iconSize: 20,
-  // 连线的粗细
-  lineWidth: 1,
-  // 连线的颜色
-  lineColor: '#549688',
-  // 连线样式
-  lineDasharray: 'none',
-  // 概要连线的粗细
-  generalizationLineWidth: 1,
-  // 概要连线的颜色
-  generalizationLineColor: '#549688',
-  // 概要曲线距节点的距离
-  generalizationLineMargin: 0,
-  // 概要节点距节点的距离
-  generalizationNodeMargin: 20,
-  // 背景颜色
-  backgroundColor: '#fafafa',
-  // 背景图片
-  backgroundImage: 'none',
-  // 背景重复
-  backgroundRepeat: 'no-repeat',
-  // 根节点样式
-  root: {
-    shape: 'rectangle',
-    fillColor: '#549688',
-    fontFamily: '微软雅黑, Microsoft YaHei',
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    lineHeight: 1.5,
-    borderColor: 'transparent',
-    borderWidth: 0,
-    borderDasharray: 'none',
-    borderRadius: 5,
-    textDecoration: 'none',
-    active: {
-      borderColor: 'rgb(57, 80, 96)',
-      borderWidth: 3,
-      borderDasharray: 'none'
-    }
-  },
-  // 二级节点样式
-  second: {
-    shape: 'rectangle',
-    marginX: 100,
-    marginY: 40,
-    fillColor: '#fff',
-    fontFamily: '微软雅黑, Microsoft YaHei',
-    color: '#565656',
-    fontSize: 16,
-    fontWeight: 'noraml',
-    fontStyle: 'normal',
-    lineHeight: 1.5,
-    borderColor: '#549688',
-    borderWidth: 1,
-    borderDasharray: 'none',
-    borderRadius: 5,
-    textDecoration: 'none',
-    active: {
-      borderColor: 'rgb(57, 80, 96)',
-      borderWidth: 3,
-      borderDasharray: 'none'
-    }
-  },
-  // 三级及以下节点样式
-  node: {
-    shape: 'rectangle',
-    marginX: 50,
-    marginY: 0,
-    fillColor: 'transparent',
-    fontFamily: '微软雅黑, Microsoft YaHei',
-    color: '#6a6d6c',
-    fontSize: 14,
-    fontWeight: 'noraml',
-    fontStyle: 'normal',
-    lineHeight: 1.5,
-    borderColor: 'transparent',
-    borderWidth: 0,
-    borderRadius: 5,
-    borderDasharray: 'none',
-    textDecoration: 'none',
-    active: {
-      borderColor: 'rgb(57, 80, 96)',
-      borderWidth: 3,
-      borderDasharray: 'none'
-    }
-  },
-  // 概要节点样式
-  generalization: {
-    shape: 'rectangle',
-    marginX: 100,
-    marginY: 40,
-    fillColor: '#fff',
-    fontFamily: '微软雅黑, Microsoft YaHei',
-    color: '#565656',
-    fontSize: 16,
-    fontWeight: 'noraml',
-    fontStyle: 'normal',
-    lineHeight: 1.5,
-    borderColor: '#549688',
-    borderWidth: 1,
-    borderDasharray: 'none',
-    borderRadius: 5,
-    textDecoration: 'none',
-    active: {
-      borderColor: 'rgb(57, 80, 96)',
-      borderWidth: 3,
-      borderDasharray: 'none'
-    }
-  }
-}); // 支持激活样式的属性
-// 简单来说，会改变节点大小的都不支持在激活时设置，为了性能考虑，节点切换激活态时不会重新计算节点大小
-
-const supportActiveStyle = ['fillColor', 'color', 'fontWeight', 'fontStyle', 'borderColor', 'borderWidth', 'borderDasharray', 'borderRadius', 'textDecoration'];
 // CONCATENATED MODULE: ../simple-mind-map/src/themes/freshGreen.js
 
 
@@ -28651,23 +28668,7 @@ class Export_Export {
 
 
   json(name, withConfig = true) {
-    let nodeData = this.mindMap.command.getCopyData();
-    let data = {};
-
-    if (withConfig) {
-      data = {
-        layout: this.mindMap.getLayout(),
-        root: nodeData,
-        theme: {
-          template: this.mindMap.getTheme(),
-          config: this.mindMap.getCustomThemeConfig()
-        },
-        view: this.mindMap.view.getTransformData()
-      };
-    } else {
-      data = nodeData;
-    }
-
+    let data = this.mindMap.getData(withConfig);
     let str = JSON.stringify(data);
     let blob = new Blob([str]);
     return URL.createObjectURL(blob);
@@ -29456,6 +29457,7 @@ const transformOldXmind = content => {
 
 
 
+
  // 默认选项配置
 
 const defaultOpt = {
@@ -29803,6 +29805,34 @@ class simple_mind_map_MindMap {
     if (data.view) {
       this.view.setTransformData(data.view);
     }
+  }
+  /** 
+   * javascript comment 
+   * @Author: 王林 
+   * @Date: 2022-09-24 14:42:07 
+   * @Desc: 获取思维导图数据，节点树、主题、布局等 
+   */
+
+
+  getData(withConfig) {
+    let nodeData = this.command.getCopyData();
+    let data = {};
+
+    if (withConfig) {
+      data = {
+        layout: this.getLayout(),
+        root: nodeData,
+        theme: {
+          template: this.getTheme(),
+          config: this.getCustomThemeConfig()
+        },
+        view: this.view.getTransformData()
+      };
+    } else {
+      data = nodeData;
+    }
+
+    return simpleDeepClone(data);
   }
   /** 
    * @Author: 王林 
