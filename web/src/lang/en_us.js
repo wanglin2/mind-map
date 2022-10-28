b46a94bd9698a49ccd51aaf5a8c34cac6d6ee7e5
wanglin2 <1013335014@qq.com>
2022-10-28 16:26:55 +0800

    多语言开发中

---
+++ web/src/lang/en_us.js
@@ -0,0 +1,80 @@
+export default {
+  baseStyle: {
+    title: 'BaseStyle',
+    background: 'Background',
+    color: 'Color',
+    image: 'Image',
+    imageRepeat: 'Image repeat',
+    line: 'Line',
+    width: 'Width',
+    style: 'Style',
+    lineOfOutline: 'Line of outline',
+    nodePadding: 'Node padding',
+    nodeMargin: 'Node margin',
+    horizontal: 'Horizontal',
+    vertical: 'Vertical',
+    maximumWidth: 'Max width',
+    maximumHeight: 'Max height',
+    icon: 'Icon',
+    size: 'Size',
+    level2Node: 'Level2 node',
+    belowLevel2Node: 'Below level2 node'
+  },
+  color: {
+    moreColor: 'More color'
+  },
+  contextmenu: {
+    insertSiblingNode: 'Insert sibling node',
+    insertChildNode: 'Insert child node',
+    insertSummary: 'Insert summary',
+    moveUpNode: 'Move up node',
+    moveDownNode: 'Move down node',
+    deleteNode: 'Delete node',
+    copyNode: 'Copy node',
+    cutNode: 'Cut node',
+    pasteNode: 'Paste node',
+    backCenter: 'Back center',
+    expandAll: 'Expand all',
+    unExpandAll: 'Un expand all',
+    expandTo: 'Expand to',
+    arrangeLayout: 'Arrange layout',
+    level1: 'Level1',
+    level2: 'Level2',
+    level3: 'Level3',
+    level4: 'Level4',
+    level5: 'Level5',
+    level6: 'Level6'
+  },
+  count: {
+    words: 'Words',
+    nodes: 'Nodes'
+  },
+  dialog: {
+    cancel: 'Cancel',
+    confirm: 'Confirm'
+  },
+  export: {
+    title: 'Export',
+    filename: 'Filename',
+    include: 'Is include config like theme and structure',
+    dedicatedFile: 'Dedicated file',
+    jsonFile: 'json file',
+    imageFile: 'Image file',
+    svgFile: 'svg file',
+    pdfFile: 'pdf file',
+    tips: 'tips：.smm and .json file can be import'
+  },
+  fullscreen: {
+    fullscreen: 'Fullscreen'
+  },
+  import: {
+    title: 'Import',
+    selectFile: 'Select file',
+    supportFile: 'Support .smm、.json、.xmind、.xlsx file'
+  },
+  navigatorToolbar: {
+    openMiniMap: 'Open mini map',
+    readonly: 'Readonly',
+    edit: 'Edit'
+  }
+}
