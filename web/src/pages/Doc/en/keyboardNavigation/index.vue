bd655839cb0c16892a5f2fd5130c920820b29a3b
wanglin2 <1013335014@qq.com>
2023-01-17 14:39:43 +0800

    提取出键盘导航类作为插件

--- web/src/pages/Doc/en/keyboardNavigation/index.vue
+++ web/src/pages/Doc/en/keyboardNavigation/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <h1>KeyboardNavigation instance</h1>
+    <h1>KeyboardNavigation plugin</h1>
 <blockquote>
 <p>v0.2.17+</p>
 </blockquote>
-<p><code>KeyboardNavigation</code> instance is responsible for handling keyboard navigation, that is, when you press the direction key, it will automatically find the next node and activate it, can be obtained through <code>mindMap.keyboardNavigation</code></p>
+<p><code>KeyboardNavigation</code> plugin provides keyboard navigation function, that is, when you press the direction key, it will automatically find the next node and activate it.</p>
+<h2>Register</h2>
+<pre class="hljs"><code><span class="hljs-keyword">import</span> MindMap <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;simple-mind-map&#x27;</span>
+<span class="hljs-keyword">import</span> KeyboardNavigation <span class="hljs-keyword">from</span> <span class="hljs-string">&#x27;simple-mind-map/src/KeyboardNavigation.js&#x27;</span>
+
+MindMap.usePlugin(KeyboardNavigation)
+</code></pre>
+<p>After registration and instantiation of <code>MindMap</code>, the instance can be obtained through <code>mindMap.keyboardNavigation</code>.</p>
 <h2>Methods</h2>
 <h3>focus(dir)</h3>
 <p><code>dir</code>：Which direction to find the next node，Optional value：<code>Left</code>、 <code>Up</code>、 <code>Right</code>、 <code>Down</code></p>
