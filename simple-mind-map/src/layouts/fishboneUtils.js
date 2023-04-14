9c60857c6ad1ae9ab656e71b1933e47f42671175
wanglin2 <1013335014@qq.com>
2023-04-14 17:43:02 +0800

    优化鱼骨结构，支持margin

--- simple-mind-map/src/layouts/fishboneUtils.js
+++ simple-mind-map/src/layouts/fishboneUtils.js
@@ -43,24 +43,26 @@ export default {
         line.plot(`M ${x},${top + height + expandBtnSize} L ${x},${maxy}`)
       }
     },
-    computedLeftTopValue({ layerIndex, node, ctx }) {
+    computedLeftTopValue({ layerIndex, node, ctx, marginY }) {
       if (layerIndex >= 1 && node.children) {
         // 遍历三级及以下节点的子节点
         let startLeft = node.left + node.width * 0.5
         let totalTop =
           node.top +
           node.height +
+          marginY +
           (ctx.getNodeActChildrenLength(node) > 0 ? node.expandBtnSize : 0)
         node.children.forEach(item => {
           item.left = startLeft
           item.top += totalTop
           totalTop +=
             item.height +
+            marginY +
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
         })
       }
     },
-    adjustLeftTopValueBefore({ node, parent, ctx }) {
+    adjustLeftTopValueBefore({ node, parent, ctx, marginY }) {
       // 调整top
       let len = node.children.length
       // 调整三级及以下节点的top
@@ -71,11 +73,11 @@ export default {
             item.height +
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
           )
-        }, 0)
+        }, 0) + len * marginY
         ctx.updateBrothersTop(node, totalHeight)
       }
     },
-    adjustLeftTopValueAfter({ parent, node, ctx }) {
+    adjustLeftTopValueAfter({ parent, node, ctx, marginY }) {
       // 将二级节点的子节点移到上方
       if (parent && parent.isRoot) {
         // 遍历二级节点的子节点
@@ -85,7 +87,7 @@ export default {
           let nodeTotalHeight = ctx.getNodeAreaHeight(item)
           let _top = item.top
           item.top =
-            node.top - (item.top - node.top) - nodeTotalHeight + node.height
+            node.top - (item.top - node.top) - nodeTotalHeight + node.height + marginY
           // 调整left
           let offsetLeft =
             (nodeTotalHeight + totalHeight) / Math.tan(degToRad(45))
@@ -133,13 +135,14 @@ export default {
         line.plot(`M ${x},${top} L ${x},${miny}`)
       }
     },
-    computedLeftTopValue({ layerIndex, node, ctx }) {
+    computedLeftTopValue({ layerIndex, node, ctx, marginY }) {
       if (layerIndex === 1 && node.children) {
         // 遍历二级节点的子节点
         let startLeft = node.left + node.width * 0.5
         let totalTop =
           node.top +
           node.height +
+          marginY +
           (ctx.getNodeActChildrenLength(node) > 0 ? node.expandBtnSize : 0)
 
         node.children.forEach(item => {
@@ -149,6 +152,7 @@ export default {
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
           totalTop +=
             item.height +
+            marginY +
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
         })
       }
@@ -157,17 +161,19 @@ export default {
         let startLeft = node.left + node.width * 0.5
         let totalTop =
           node.top -
+          marginY -
           (ctx.getNodeActChildrenLength(node) > 0 ? node.expandBtnSize : 0)
         node.children.forEach(item => {
           item.left = startLeft
           item.top = totalTop - item.height
           totalTop -=
             item.height +
+            marginY +
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
         })
       }
     },
-    adjustLeftTopValueBefore({ node, ctx, layerIndex }) {
+    adjustLeftTopValueBefore({ node, ctx, layerIndex, marginY }) {
       // 调整top
       let len = node.children.length
       if (layerIndex > 2 && len > 0) {
@@ -177,11 +183,12 @@ export default {
             item.height +
             (ctx.getNodeActChildrenLength(item) > 0 ? item.expandBtnSize : 0)
           )
-        }, 0)
+        }, 0) + 
+        len * marginY
         ctx.updateBrothersTop(node, -totalHeight)
       }
     },
-    adjustLeftTopValueAfter({ parent, node, ctx }) {
+    adjustLeftTopValueAfter({ parent, node, ctx, marginY }) {
       // 将二级节点的子节点移到上方
       if (parent && parent.isRoot) {
         // 遍历二级节点的子节点
@@ -198,7 +205,7 @@ export default {
                 (hasChildren ? item.expandBtnSize : 0)
               : 0
           let _top = totalHeight + offset
-          item.top += _top
+          item.top += _top - marginY
           // 调整left
           let offsetLeft =
             (totalHeight2 + nodeTotalHeight) / Math.tan(degToRad(45))
