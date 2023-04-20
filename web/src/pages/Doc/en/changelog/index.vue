b91dde8084407ab2b6e8567648572308f879a762
wanglin2 <1013335014@qq.com>
2023-04-20 16:31:28 +0800

    更新文档

--- web/src/pages/Doc/en/changelog/index.vue
+++ web/src/pages/Doc/en/changelog/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h1>Changelog</h1>
+<h2>0.5.6</h2>
+<p>Fix: 1.Fix the issue of node position disorder during fast and multiple renderings in a short period of time. 2.Fix the issue of dragging the canvas while the node is being edited, causing the edit box and node to separate.</p>
+<p>New: 1.Add a maximum history limit.</p>
 <h2>0.5.5-fix.1</h2>
 <p>Fix: 1.Fix the issue where the edit box is also outside the canvas when editing nodes outside the canvas. 2.After modifying the structure, reset the transformation to prevent the problem of sudden position changes during the first drag after switching the structure during scaling.</p>
 <p>optimization: 1.When multiple nodes are selected, as long as there is a cross between the node and the selection area, it is considered selected.</p>
+<h2>0.5.5-fix.2</h2>
+<p>Fix: 1.Fix mini map error.</p>
 <h2>0.5.5</h2>
 <p>New: 1.Supports configuring the padding when exporting to PNG, SVG, or PDF. 2.Support the configuration of z-index for node text editing boxes and node comment floating layer elements. 3.Support clicking on areas outside the canvas to end node editing status.</p>
 <h2>0.5.4</h2>
