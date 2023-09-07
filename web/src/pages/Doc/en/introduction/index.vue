305c5e9f7091579c1831c7b6bdf43a8996277a51
wanglin2 <1013335014@qq.com>
2023-09-07 17:36:36 +0800

    Doc: update

--- web/src/pages/Doc/en/introduction/index.vue
+++ web/src/pages/Doc/en/introduction/index.vue
@@ -151,6 +151,10 @@ full screen, support mini map</li>
         <img src="../../../../assets/avatar/南风.jpg" style="width: 50px;height: 50px;object-fit: cover;border-radius: 50%;" />
         <p>南风</p>
     </div>
+    <div style="display: flex; flex-direction: column; align-items: center; width: fit-content; margin: 5px;">
+        <img src="../../../../assets/avatar/蜉蝣撼大叔.jpg" style="width: 50px;height: 50px;object-fit: cover;border-radius: 50%;" />
+        <p>蜉蝣撼大叔</p>
+    </div>
 </div>
   </div>
 </template>
