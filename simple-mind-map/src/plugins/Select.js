740e2e341079a8caca8b48746edf0f0f47570342
wanglin2 <1013335014@qq.com>
2023-09-21 09:00:25 +0800

    Fix：修复多选节点时选区未包含节点边界时节点不会被选中的问题

--- simple-mind-map/src/plugins/Select.js
+++ simple-mind-map/src/plugins/Select.js
@@ -1,4 +1,4 @@
-import { bfsWalk, throttle } from '../utils'
+import { bfsWalk, throttle, checkTwoRectIsOverlap } from '../utils'
 
 // 节点选择插件
 class Select {
@@ -210,24 +210,19 @@ class Select {
       left = left * scaleX + translateX
       top = top * scaleY + translateY
       if (
-        ((left >= minx && left <= maxx) || (right >= minx && right <= maxx)) &&
-        ((top >= miny && top <= maxy) || (bottom >= miny && bottom <= maxy))
+        checkTwoRectIsOverlap(minx, maxx, miny, maxy, left, right, top, bottom)
       ) {
-        // this.mindMap.batchExecution.push('activeNode' + node.uid, () => {
         if (node.nodeData.data.isActive) {
           return
         }
         this.mindMap.renderer.setNodeActive(node, true)
         this.mindMap.renderer.addActiveNode(node)
-        // })
       } else if (node.nodeData.data.isActive) {
-        // this.mindMap.batchExecution.push('activeNode' + node.uid, () => {
         if (!node.nodeData.data.isActive) {
           return
         }
         this.mindMap.renderer.setNodeActive(node, false)
         this.mindMap.renderer.removeActiveNode(node)
-        // })
       }
     })
   }
