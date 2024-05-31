67fec82c728213cd80ec815fb7ea5d52013b3515
街角小林 <1013335014@qq.com>
2024-05-31 15:57:09 +0800

    update

--- simple-mind-map/src/plugins/RichText.js
+++ simple-mind-map/src/plugins/RichText.js
@@ -313,6 +313,8 @@ class RichText {
   getEditText() {
     let html = this.quill.container.firstChild.innerHTML
     // 去除ql-cursor节点
+    // https://github.com/wanglin2/mind-map/commit/138cc4b3e824671143f0bf70e5c46796f48520d0
+    // https://github.com/wanglin2/mind-map/commit/0760500cebe8ec4e8ad84ab63f877b8b2a193aa1
     // html = removeHtmlNodeByClass(html, '.ql-cursor')
     // 去除最后的空行
     return html.replace(/<p><br><\/p>$/, '')
