b8a23beba47d12475881264e24391fcf5a1cac82
街角小林 <1013335014@qq.com>
2024-06-04 11:47:34 +0800

    Feat：打包后的文件中增加演示插件

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -16,6 +16,7 @@ import Painter from './src/plugins/Painter.js'
 import Scrollbar from './src/plugins/Scrollbar.js'
 import Formula from './src/plugins/Formula.js'
 import RainbowLines from './src/plugins/RainbowLines.js'
+import Demonstrate from './src/plugins/Demonstrate.js'
 import xmind from './src/parse/xmind.js'
 import markdown from './src/parse/markdown.js'
 import icons from './src/svg/icons.js'
@@ -48,5 +49,6 @@ MindMap.usePlugin(MiniMap)
   .usePlugin(Scrollbar)
   .usePlugin(Formula)
   .usePlugin(RainbowLines)
+  .usePlugin(Demonstrate)
 
 export default MindMap
