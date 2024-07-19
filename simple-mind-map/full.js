1b4ca19ad8788398e175f15101ffc26db94176f9
街角小林 <1013335014@qq.com>
2024-07-19 14:22:00 +0800

    打包0.10.3

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -31,7 +31,7 @@ MindMap.iconList = icons.nodeIconList
 MindMap.constants = constants
 MindMap.themes = themes
 MindMap.defaultTheme = defaultTheme
-MindMap.version = '0.10.2-fix.1'
+MindMap.version = '0.10.3'
 
 MindMap.usePlugin(MiniMap)
   .usePlugin(Watermark)
