ac13aa8bc9a4ba550012188dbf928827888e4325
街角小林 <1013335014@qq.com>
2024-09-11 19:59:59 +0800

    Doc: update

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -31,7 +31,7 @@ MindMap.iconList = icons.nodeIconList
 MindMap.constants = constants
 MindMap.themes = themes
 MindMap.defaultTheme = defaultTheme
-MindMap.version = '0.11.0'
+MindMap.version = '0.11.1'
 
 MindMap.usePlugin(MiniMap)
   .usePlugin(Watermark)
