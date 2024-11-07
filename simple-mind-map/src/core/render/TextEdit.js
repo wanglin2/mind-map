2078d380924e592bbc9916736e75067ce169f634
街角小林 <1013335014@qq.com>
2024-11-07 09:08:04 +0800

    update

--- simple-mind-map/src/core/render/TextEdit.js
+++ simple-mind-map/src/core/render/TextEdit.js
@@ -289,6 +289,9 @@ export default class TextEdit {
         outline: none; 
         word-break: break-all;
         line-break: anywhere;
+        transform-style: preserve-3d;
+        filter: contrast(1) saturate(1);
+        backface-visibility: hidden;
       `
       this.textEditNode.setAttribute('contenteditable', true)
       this.textEditNode.addEventListener('keyup', e => {
